655114992ab5ed093d6d496a014e0dbe4c72d45c
你好2007 <2501482523@qq.com>
2021-04-26 15:02:59 +0800

    修复DOM操作方法toNode对一些特殊标签的支持

--- xhtml.js
+++ xhtml.js
@@ -138,15 +138,37 @@ export default {
 
     // 字符串变成结点
     // isSvg可选，boolean值，默认false表示结点是html，为true表示svg类型
-    "toNode": function (string, isSvg) {
+    "toNode": function (template, isSvg) {
         var frame;
 
         // html和svg上下文不一样
         if (isSvg) frame = document.createElementNS(namespace.svg, 'svg');
-        else frame = document.createElement("div");
+        else {
+
+            var frameTagName = 'div';
+
+            // 大部分的标签可以直接使用div作为容器
+            // 部分特殊的需要特殊的容器标签
+
+            if (/^<tr[> ]/.test(template)) {
+
+                frameTagName = "tbody";
+
+            } else if (/^<th[> ]/.test(template) || /^<td[> ]/.test(template)) {
+
+                frameTagName = "tr";
+
+            } else if (/^<thead[> ]/.test(template) || /^<tbody[> ]/.test(template)) {
+
+                frameTagName = "table";
+
+            }
+
+            frame = document.createElement(frameTagName);
+        }
 
         // 低版本浏览器svg没有innerHTML，考虑是vue框架中，没有补充
-        frame.innerHTML = string;
+        frame.innerHTML = template;
 
         var childNodes = frame.childNodes;
         for (var i = 0; i < childNodes.length; i++) {
